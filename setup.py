8b0cabfe0b4e223d84d0a6690d663bf7bbbdebd5
Nick Heppert <nick.heppert@gmx.de>
2019-05-01 15:12:12 +0900

    Fix typo

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from catkin_pkg.python_setup import generate_distutils_setup
 
 d = generate_distutils_setup(
     packages=["octomap_flatter"],
-    scripts=["scripts/boundary"_detector"],
+    scripts=["scripts/boundary_detector"],
     package_dir={"": "src"}
 )
 
