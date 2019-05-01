950619f3ad3816ebaab47d64d5679e707fe2d958
Nick Heppert <nick.heppert@gmx.de>
2019-05-01 15:02:25 +0900

    Change ' to "

--- setup.py
+++ setup.py
@@ -2,9 +2,9 @@ from distutils.core import setup
 from catkin_pkg.python_setup import generate_distutils_setup
 
 d = generate_distutils_setup(
-    packages=['octomap_flatter'],
-    scripts=['scripts/boundary_detector'],
-    package_dir={'': 'src'}
+    packages=["octomap_flatter"],
+    scripts=["scripts/boundary"_detector"],
+    package_dir={"": "src"}
 )
 
 setup(**d)
