ab7a9a9cb27b78935aa8354a8c3ea61cf20b4224
Nick Heppert <nick.heppert@gmx.de>
2019-05-29 17:34:34 +0900

    Implement Median Filter for removing reflection

--- scripts/depth_image_filter_node.py
+++ scripts/depth_image_filter_node.py
@@ -16,8 +16,15 @@ class SubThenFilter:
         self.sub = rospy.Subscriber(sub_topic, Image, self.image_callback, queue_size=1)
         self.pub = rospy.Publisher(pub_topic, Image, queue_size=1)
         self.bridge = CvBridge()
-        self.median_blur_size = 5
         self.use_median_blur = True
+        self.median_blur_size = 5
+        self.use_inpaiting = False
+        self.inpaiting_iterations = 5
+        # self.use_adaptive_threshold = True
+        self.manual_threshold = False
+        self.filtering_iterations = 3
+
+        self.max_threshold_uint16 = 2000
 
     def image_callback(self, data):
         try:
@@ -26,8 +33,71 @@ class SubThenFilter:
             print(e)
 
         cv_image = np.nan_to_num(cv_image)
+
+        min_val, max_val, _, _ = cv2.minMaxLoc(cv_image)
+        print("===== In\n{}\n=====".format((min_val, max_val)))
+
+        uint16_to_uint8_scaling = 255.0/max_val
+        cv_image = cv2.convertScaleAbs(cv_image, alpha=uint16_to_uint8_scaling)
+        max_threshold_uint8 = self.max_threshold_uint16 * uint16_to_uint8_scaling
+        print(max_threshold_uint8)
+
+        if self.use_inpaiting:
+            iterations = 0
+            while True and iterations < self.inpaiting_iterations:
+                mask = cv_image > max_threshold_uint8
+                if not mask.any():
+                    break
+                mask = mask.astype(np.uint8)
+                cv_image = cv2.inpaint(cv_image , mask, 11, cv2.INPAINT_NS)
+                iterations += 1
+            print("Inpaiting iterations: {}".format(iterations))
+
+        if self.manual_threshold:
+            # Slow
+            mask = cv_image > max_threshold_uint8
+            indices = np.where(mask)
+
+            for x, y in zip(indices[0], indices[1]):
+                min_ = max_threshold_uint8 + 1
+                filter_size = 2
+                iterations = 0
+                while min_ > max_threshold_uint8 and iterations < self.filtering_iterations:
+                    for x_a in range(x - filter_size, x + filter_size):
+                        for y_a in range(y - filter_size, y + filter_size):
+                            min_ = np.min(cv_image[x_a][y_a])
+                    filter_size *= 2
+                    iterations += 1
+                # print(min_)
+                cv_image[x][y] = min_
+
         if self.use_median_blur:
-            cv_image = cv2.medianBlur(cv_image, self.median_blur_size)
+            # cv_image = cv2.medianBlur(cv_image, self.median_blur_size)
+            # while True:
+            for filter_size in [201, 101, 51, 33, 19, 5, 3]:
+                mask = cv_image > max_threshold_uint8
+                if not mask.any():
+                    break
+                indices = np.where(mask)
+                cv_image_2 = cv2.medianBlur(cv_image, filter_size)
+                cv_image[indices] = cv_image_2[indices]
+            cv_image = cv2.medianBlur(cv_image, 5)
+
+        # if self.use_adaptive_threshold:
+        #     cv_image = cv2.adaptiveThreshold(cv_image, max_threshold_uint8, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 11, 2) # 3 = Meters
+
+        cv2.imshow('image', cv_image)
+        cv2.waitKey(1)
+
+        min_val, max_val, _, _ = cv2.minMaxLoc(cv_image)
+        print("===== uint8\n{}\n=====".format((min_val, max_val)))
+
+        cv_image = cv_image.astype(np.float64)
+        cv_image /= uint16_to_uint8_scaling
+        cv_image = cv_image.astype(np.uint16)
+
+        min_val, max_val, _, _ = cv2.minMaxLoc(cv_image)
+        print("===== Out\n{}\n=====".format((min_val, max_val)))
 
         try:
             msg = self.bridge.cv2_to_imgmsg(cv_image, "passthrough")
